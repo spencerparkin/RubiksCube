6f590a6cb23f9abde5f9270475c20da1c74f7c73
Spencer T. Parkin <spencerparkin@outlook.com>
2013-11-26 00:24:32 -0700

    i think it's doable, but quite a bit of work

--- Code/SolverForCase3.cpp
+++ Code/SolverForCase3.cpp
@@ -104,16 +104,40 @@ void SolverForCase3::PerformCubeOrientingStage( const RubiksCube* rubiksCube, Ru
 //==================================================================================================
 void SolverForCase3::PerformRedCrossPositioningStage( const RubiksCube* rubiksCube, RubiksCube::RotationSequence& rotationSequence )
 {
-	// Find the Red/Yellow edge peice.
-	// If it is not already in position.
-	//   If it is in the Z=2 plane, rotate it to the Z=0 plane, return.
-	//   If it is in the Z=0 plane, rotate it in that plane to the right spot.
-	//   If it was already in the right spot
+	// For each edge piece,
+	//   If it is not already in position.
+	//     If it is in the Z=2 plane, rotate it to the Z=0 plane, return.
+	//     If it is in the Z=0 plane and not in the right face plane, rotate it in that plane to the right face plane.
+	//     If it is in the Z=0 plane and in the right face plane, rotate it up into the Z=0 plane.
 }
 
 //==================================================================================================
 void SolverForCase3::PerformRedCrossOrientingStage( const RubiksCube* rubiksCube, RubiksCube::RotationSequence& rotationSequence )
 {
+	// If the Red/Yellow edge piece is not oriented properly,
+	//   choose rAxis = xAxis, uAxis = zAxis, fAxis = -yAxis, then do Ri, U, Fi, Ui.
+	// If the Red/Blue edge piece is not oriented properly,
+	//   choose rAxis = yAxis, uAxis = zAxis, fAxis = xAxis, then do Ri, U, Fi, Ui.
+	// If the Red/White edge piece is not oriented properly,
+	//   choose rAxis = -xAxis, uAxis = zAxis, fAxis = yAxis, then do Ri, U, Fi, Ui.
+	// If the Red/Green edge piece is not oriented properly,
+	//   choose rAxis = -yAxis, uAxis = zAxis, fAxis = -xAxis, then do Ri, U, Fi, Ui.
 }
+
+//void SolverForCase3::PerformRedFaceCornersPositioningStage()
+//{
+	// For each corner piece,
+	//   If it is not already in position,
+	//     If it is in the Z=2 plane, do a sequence to get it out to Z=0 plane without disturbing what we have
+	//     If it is in Z=0 plane, but not the right corner, get it into the right corner
+	//     If it is in Z=0 plane and the right corner, sequence it up into position.
+//}
+
+//void SolverFromCase3::PerformRedFaceCornerOrientingStage()
+//{
+	// For each corner piece,
+	//   If not already in right orientation
+	//     Choose correct perspective, then do (Ri, Di, R, D)x2
+//}
 
 // SolverForCase3.cpp
